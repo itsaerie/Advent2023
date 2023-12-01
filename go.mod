1d0e5960fb861681510e0b034b1d3a818a206ade
Simon Lam <sl755@njit.edu>
2023-11-30 23:52:21 -0500

    Init
    Setting up my golang workflow. Time to learn golang!

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/itsaerie/Advent2023
+
+go 1.21.4
---
+++ launcher.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func partOne(file *os.File) {
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func partTwo(file *os.File) {
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	// open input file
+	file, _ := os.Open(os.Args[1] + ".txt")
+	defer file.Close()
+
+	// arg parsing for which day to run; default to copy_paste
+	part1 := partOne
+	part2 := partTwo
+
+	switch day := os.Args[1]; day {
+	case "One":
+		part1 = OneOne
+		part2 = OneTwo
+	default:
+		part1 = partOne
+		part2 = partTwo
+	}
+
+	// arg parsing for part 1 or part 2; default to part 1
+	switch part := os.Args[2]; part {
+	case "1":
+		part1(file)
+	case "2":
+		part2(file)
+	default:
+		part1(file)
+		part2(file)
+	}
+}
